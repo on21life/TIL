d3f177fbe1232754e64817ef953bd97949746b34
윤석현 <on21life@naver.com>
2018-11-05 14:02:38 +0900

    Setting GIF engine

---
+++ giphy-search-engine/main.js
@@ -0,0 +1,6 @@
+// 1. <input> 태그 안의 값을 잡는다.
+const inputValue = document.querySelector('#js-input').value;
+document.write(inputValue);
+console.log(inputValue)
+// 2. API 를 활용해서 data를 받고 가공한다.
+// 3. GIF 파일들을 index.html에 밀어 넣는다.
