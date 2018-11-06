b1848498df1542887190fe730ca6b03898b985f0
윤석현 <on21life@naver.com>
2018-11-06 09:48:24 +0900

    for sound cloud setting

---
+++ sound-cloud-player/README.md
@@ -0,0 +1,6 @@
+# Sound Cloud Player
+
+```sh
+$ curl -OL  https://github.com/neovansoarer/neovansoarer.github.io/blob/master/files/sound-cloud-player.zip  
+```
+
---
+++ sound-cloud-player/javascript/main.js
@@ -0,0 +1,28 @@
+/* 1. 검색 */
+
+/* 2. SoundCloud API  사용하기 */
+const SoundCloudAPI = {
+  init: () => {
+    SC.initialize({
+      client_id: "cd9be64eeb32d1741c17cb39e41d254d"
+    });
+  },
+
+  getTrack: (inputValue) => {
+    SC.get("/tracks", {
+      q: inputValue,
+      license: ""
+    }).then(function(tracks) {
+      console.log(tracks);
+    });
+  }
+};
+
+SoundCloudAPI.init();
+SoundCloudAPI.getTrack("coffee");
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+
+/* 3. 카드 보여주기 */
+
+/* 4. Playlist 에 추가하고 실제로 재생하기 */
