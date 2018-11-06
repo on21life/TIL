707e98d1891aae80e4cee2ebf5d17335092caf25
윤석현 <on21life@naver.com>
2018-11-06 11:39:35 +0900

    before adding playlist

--- sound-cloud-player/javascript/main.js
+++ sound-cloud-player/javascript/main.js
@@ -8,78 +8,84 @@ const SoundCloudAPI = {
     });
   },
 
-  getTrack: (inputValue) => {
+  getTrack: inputValue => {
     SC.get("/tracks", {
       q: inputValue,
       license: ""
     }).then(function(tracks) {
       console.log(tracks);
+      SoundCloudAPI.renderTracks(tracks);
     });
-  },
+  }
 };
 
 SoundCloudAPI.init();
-SoundCloudAPI.getTrack("coffee");
+SoundCloudAPI.getTrack("busker");
 // find all sounds of buskers licensed under 'creative commons share alike'
 
 /* 3. 카드 보여주기 */
-SoundCloudAPI.renderTracks = () => {
-  // tracks.forEach(()=>{});
-  // Card
-  const card = document.createElement('div');
-  card.classList.add("card");
-  // class.List.add는 클래스 추가, createElement는 태그 생성.
-  console.log(card);
+SoundCloudAPI.renderTracks = (tracks) => {
+  tracks.forEach((track) => {
+    // Card
+    const card = document.createElement("div");
+    card.classList.add("card");
+    // class.List.add는 클래스 추가, createElement는 태그 생성.
+    console.log(card);
 
-  // Image
-  const imageDiv = document.createElement('div');
-  imageDiv.classList.add('image')
-  
-  
-  const imageImg = document.createElement('img');
-  imageImg.classList.add('image_img');
-  imageImg.src = 'http://www.placekitten.com/290/290';
-  imageDiv.appendChild(imageImg);
+    // Image
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("image");
 
-  // Content
-  const content = document.createElement('div');
-  content.classList.add("content")
+    const imageImg = document.createElement("img");
+    imageImg.classList.add("image_img");
+    imageImg.src = (track.artwork_url || "http://lorempixel.com/100/100/abstract/");
 
-  const header = document.createElement('div');
-  header.classList.add('header')
+    imageDiv.appendChild(imageImg);
+    
 
-  const link = document.createElement('a');
-  link.href = "http://lorempixel.com/100/100/abstract/"
-  link.target = "_blank"
-  link.innerHTML = "this hits"
+    // Content
+    const content = document.createElement("div");
+    content.classList.add("content");
 
-  // Button
-  const button = document.createElement('div');
-  button.classList.add('ui', 'bottom','attached','button','js-button')
-  
-  const icon = document.createElement('i');
-  icon.classList.add('add','icon');
+    const header = document.createElement("div");
+    header.classList.add("header");
 
-  const buttonText = document.createElement('span');
-  buttonText.innerHTML = 'Add to Playlist';
+    const link = document.createElement("a");
+    link.href = track.permalink_url;
+    link.target = "_blank";//new tab
+    link.innerHTML = track.title;
 
+    // Button
+    const button = document.createElement("div");
+    button.classList.add("ui", "bottom", "attached", "button", "js-button");
 
-  // 조립
-  imageDiv.appendChild(imageImg);
+    const icon = document.createElement("i");
+    icon.classList.add("add", "icon");
 
-  header.appendChild(link);
-  content.appendChild(header);
+    const buttonText = document.createElement("span");
+    buttonText.innerHTML = "Add to Playlist";
 
-  button.appendChild(icon);
-  button.appendChild(buttonText);
+    // 조립
+    imageDiv.appendChild(imageImg);
 
-  card.appendChild(imageDiv);
-  card.appendChild(content);
-  card.appendChild(button);
+    header.appendChild(link);
+    content.appendChild(header);
 
-  const searchResults = document.querySelector('#js-search-results');
-  searchResults.appendChild(card);
-}
-let eaching = SoundCloudAPI.getTrack("coffee");
-SoundCloudAPI.renderTracks();
+    button.appendChild(icon);
+    button.appendChild(buttonText);
+
+    card.appendChild(imageDiv);
+    card.appendChild(content);
+    card.appendChild(button);
+
+    const searchResults = document.querySelector("#js-search-results");
+    searchResults.appendChild(card);
+  });
+};
+
 /* 4. Playlist 에 추가하고 실제로 재생하기 */
+SC.oEmbed('http://soundcloud.com/forss/flickermood', {
+  auto_play: true
+}).then(function(embed){
+  console.log('oEmbed response: ', embed);
+});
